43fcbfba65d1219316deeefd8ed785c84ea566fd
Marc Bluemner <Marc.Bluemner@mspag.com>
2014-05-27 06:21:56 +0200

    initial commit

---
+++ app/models/to_do.js
@@ -0,0 +1,51 @@
+var ToDo = function () {
+
+  this.defineProperties({
+    title: {type: 'string', required: true},
+    status: {type: 'string'}
+  });
+
+  this.validatesPresent('title');
+  this.validatesLength('title', {min: 5});
+
+  this.validatesWithFunction('status', function(status) {
+    return status == 'open' || status == 'done';
+  }, {message: "Status must be 'open' or 'done.'"});
+
+  /*
+  this.property('login', 'string', {required: true});
+  this.property('password', 'string', {required: true});
+  this.property('lastName', 'string');
+  this.property('firstName', 'string');
+
+  this.validatesPresent('login');
+  this.validatesFormat('login', /[a-z]+/, {message: 'Subdivisions!'});
+  this.validatesLength('login', {min: 3});
+  // Use with the name of the other parameter to compare with
+  this.validatesConfirmed('password', 'confirmPassword');
+  // Use with any function that returns a Boolean
+  this.validatesWithFunction('password', function (s) {
+      return s.length > 0;
+  });
+
+  // Can define methods for instances like this
+  this.someMethod = function () {
+    // Do some stuff
+  };
+  */
+
+};
+
+/*
+// Can also define them on the prototype
+ToDo.prototype.someOtherMethod = function () {
+  // Do some other stuff
+};
+// Can also define static methods and properties
+ToDo.someStaticMethod = function () {
+  // Do some other stuff
+};
+ToDo.someStaticProperty = 'YYZ';
+*/
+
+ToDo = geddy.model.register('ToDo', ToDo);
